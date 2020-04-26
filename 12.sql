243bc5123d4225fdeb5fea0a07609f136134e9f0
raghavnautiyal <60848671+raghavnautiyal@users.noreply.github.com>
2020-04-26 19:53:27 +0530

    Delete 12.sql

--- 12.sql
+++
@@ -1,8 +0,0 @@
-SELECT movies.title FROM people
-JOIN stars ON people.id = stars.person_id
-JOIN movies ON stars.movie_id = movies.id
-WHERE name = "Johnny Depp" AND movies.title
-IN (SELECT movies.title FROM people
-JOIN stars ON people.id = stars.person_id
-JOIN movies ON stars.movie_id = movies.id
-WHERE name = "Helena Bonham Carter");
