58a0307e44c6f64d3385780151625fe5a0a13ec7
raghavnautiyal <60848671+raghavnautiyal@users.noreply.github.com>
2020-04-26 19:53:14 +0530

    Delete 13.sql

--- 13.sql
+++
@@ -1,7 +0,0 @@
-SELECT DISTINCT (name) FROM people
-WHERE name IS NOT 'Kevin Bacon'
-AND id IN
-(SELECT person_id FROM stars Where movie_id IN
-(SELECT movie_id FROM stars Where person_id IN
-(SELECT id FROM people WHERE name IS 'Kevin Bacon' and birth = 1958)))
-ORDER BY name;
