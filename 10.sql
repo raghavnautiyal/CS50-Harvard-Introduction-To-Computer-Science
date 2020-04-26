39ca40c02617829e22829a9cd377fcd35e21c7b9
raghavnautiyal <60848671+raghavnautiyal@users.noreply.github.com>
2020-04-26 19:53:54 +0530

    Delete 10.sql

--- 10.sql
+++
@@ -1,4 +0,0 @@
-SELECT DISTINCT name FROM people 
-JOIN movies, directors, ratings 
-ON movies.id = ratings.movie_id AND movies.id = directors.movie_id AND people.id = directors.person_id 
-WHERE rating >= 9.0;
