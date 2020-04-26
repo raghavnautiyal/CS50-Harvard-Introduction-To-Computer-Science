66abba903b4862530ac9c1e2f1965021c0e04a50
raghavnautiyal <60848671+raghavnautiyal@users.noreply.github.com>
2020-04-26 19:53:38 +0530

    Delete 11.sql

--- 11.sql
+++
@@ -1,5 +0,0 @@
-SELECT title FROM movies JOIN people, stars, ratings
-ON movies.id = ratings.movie_id AND movies.id = stars.movie_id AND people.id = stars.person_id
-WHERE name = "Chadwick Boseman"
-ORDER BY rating DESC
-LIMIT 5;
